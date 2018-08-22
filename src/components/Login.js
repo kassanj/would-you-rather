d951aa83246cdf187ee7692afa4001f6ac03e777
kassanj <kassandra@whereby.us>
2018-08-22 14:25:03 -0700

    add Login component

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Login extends Component {
+  render() {
+
+    return (
+      <div>
+         <b>LOGIN HERE</b>
+      </div>
+    )
+  }
+}
+
+export default connect()(Login)
