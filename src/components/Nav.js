5e64f67d9ee9223d152a3bc73190c58c58786726
kassanj <kassandra@whereby.us>
2018-08-22 13:53:58 -0700

    feat: add html to Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+ export default function Nav () {
+
+  return (
+    <nav className='nav'>
+      <ul>
+        <li>
+            Home
+        </li>
+        <li>
+            New Question
+        </li>
+      </ul>
+    </nav>
+  )
+}
