d24bb2e4f1e18bde4b43af85fcca9084e2560b3a
kassanj <kassandra@whereby.us>
2018-08-22 14:23:05 -0700

    feat: create Home component for landing page

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Home extends Component {
+  render() {
+
+    const { questionIds } = this.props
+
+    return (
+      <div>
+        <h3 className='center'>Questions</h3>
+        <ul className='dashboard-list'>
+
+          {questionIds.map((id) => (
+            <li key={id}>
+              <div>QUESTION ID: {id}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+ function mapStateToProps ({ questions }) {
+  return {
+    questionIds: Object.keys(questions)
+      .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+  }
+}
+export default connect(mapStateToProps)(Home)
