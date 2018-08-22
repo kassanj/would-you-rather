b56024e4c523265064445487255416cc21e0c047
kassanj <kassandra@whereby.us>
2018-08-22 13:55:11 -0700

    fix: Nav and Leaderboard to main Dashboard component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,31 +1,19 @@
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import Nav from './Nav'
+import Leaderboard from './Leaderboard'
 
 
 class Dashboard extends Component {
+
   render() {
     return (
       <div>
-        <h3 className='center'>Questions</h3>
-        <ul className='dashboard-list'>
-
-          {this.props.questionAuthors}
-          {this.props.questionIds.map((id) => (
-            <li key={id}>
-              <div>QUESTION ID: {id}</div>
-            </li>
-          ))}
-        </ul>
+        <Nav />
+        <Leaderboard />
       </div>
     )
   }
 }
- function mapStateToProps ({ questions }) {
-  return {
-    questionIds: Object.keys(questions)
-      .sort((a,b) => questions[b].timestamp - questions[a].timestamp),
-    questionAuthors: Object.keys(questions).map(question => question.author)
-  }
-}
-export default connect(mapStateToProps)(Dashboard)
+
+export default connect()(Dashboard)
