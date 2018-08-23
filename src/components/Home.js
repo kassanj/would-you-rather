3a3e5a9accfe29a26c9f6b892c4142cffaf56c51
kassanj <kassandra@whereby.us>
2018-08-23 10:22:04 -0700

    fix unanswered and answered question logic

--- src/components/Home.js
+++ src/components/Home.js
@@ -33,21 +33,22 @@ class Home extends Component {
   }
 }
 
-function mapStateToProps ({ authedUser, questions }) {
+function mapStateToProps ({ authedUser, questions, users }) {
   return {
     ...questions,
+    ...users,
     authedUser,
-    
+
     answeredQuestions: Object.keys(questions)
       .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
-      .filter( a => questions[a].optionOne.votes.includes(authedUser) ||
-     questions[a].optionTwo.votes.includes(authedUser))
+      .filter( a => questions[a].optionOne.votes.includes(users[authedUser].id) ||
+     questions[a].optionTwo.votes.includes(users[authedUser].id))
       .map((a) => questions[a] ),
 
     unansweredQuestions: Object.keys(questions)
        .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
-       .filter( a => questions[a].optionOne.votes.indexOf(authedUser) == -1 &&
-      questions[a].optionTwo.votes.indexOf(authedUser) == -1 )
+       .filter( a => questions[a].optionOne.votes.indexOf(users[authedUser].id) == -1 &&
+      questions[a].optionTwo.votes.indexOf(users[authedUser].id) == -1 )
       .map((a) => questions[a] )
   }
 }
