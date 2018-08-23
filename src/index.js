188b91672bf3577f5f0e8540f1924e734ad95e3e
kassanj <kassandra@whereby.us>
2018-08-23 11:25:38 -0700

    feat: add redux-persist package and configure

--- src/index.js
+++ src/index.js
@@ -4,15 +4,29 @@ import './index.css';
 import App from './components/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers'
+
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import reducers from './reducers'
 import middleware from './middleware'
 
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-const store = createStore(reducer, middleware)
+const pReducer = persistReducer(persistConfig, reducers)
+const store = createStore(pReducer, middleware)
+const persistor = persistStore(store)
+
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 document.getElementById('root')
 )
