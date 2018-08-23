583b3bae8612417e582bea2376a246e45c732a80
kassanj <kassandra@whereby.us>
2018-08-23 10:20:32 -0700

    add addQuestion and handleAddQuestion action creators

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,4 +1,30 @@
+import { saveQuestion } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
+
+function addQuestion (question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  }
+}
+
+export function handleAddQuestion (opt1, opt2) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+
+    dispatch(showLoading())
+    return saveQuestion({
+      opt1,
+      opt2,
+      author: authedUser,
+    })
+      .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()))
+  }
+}
 
 export function receiveQuestions (questions) {
   return {
