09e625d18ca4eee29932d20548716cc9f06d8c9c
kassanj <kassandra@whereby.us>
2018-08-24 14:15:15 -0700

    add switch case to render view depending on Question status prop

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import PollResults from './QuestionViews/PollResults'
+import PollVoting from './QuestionViews/PollVoting'
+import FeedView from './QuestionViews/FeedView'
 
 class Question extends Component {
 
   render() {
 
-    const question = this.props.question
+    const { id, question, status } = this.props
 
-    return (
+    switch (status) {
+       case 'PollResults':
+         return (
+           <PollResults
+             id={id}
+             question={question}
+           />
+         );
+       case 'PollVoting':
+         return (
+           <PollVoting
+             id={id}
+             question={question}
+           />
+         );
+       case 'GeneralView':
+         return (
+           <FeedView
+             id={id}
+             question={question}
+             status={status}
+           />
+         );
+       default:
+         return (
+           <FeedView
+             id={id}
+             question={question}
+             status={status}
+           />
+         );
+       }
 
-      <div>
-        <li key={question.id}>
-          <Link to={`/question/${question.id}`} exact='true' activeclassname='active'>
-          <div>QUESTION ID: {question.id}</div>
-          <div>{question.author}</div>
-          <div>{new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit'}).format(question.timestamp)}</div>
-          </Link>
-        </li>
-      </div>
-    )
+
   }
 }
 
