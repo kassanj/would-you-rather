42ca180351928bcf547e1ec7a9a1972e977c9c16
kassanj <kassandra@whereby.us>
2018-08-24 12:52:45 -0700

    fix: remove react-redux-persist due to loader error

--- src/index.js
+++ src/index.js
@@ -5,31 +5,14 @@ import App from './components/App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import { PersistGate } from 'redux-persist/integration/react'
-import { persistStore, persistReducer } from 'redux-persist'
-
-import hardSet from 'redux-persist/lib/stateReconciler/hardSet'
-import storage from 'redux-persist/lib/storage'
-
 import reducers from './reducers'
 import middleware from './middleware'
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  stateReconciler: hardSet,
-}
-
-const pReducer = persistReducer(persistConfig, reducers)
-const store = createStore(pReducer, middleware)
-const persistor = persistStore(store)
-
+const store = createStore(reducers, middleware)
 
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
+    <App />
   </Provider>,
 document.getElementById('root')
 )
