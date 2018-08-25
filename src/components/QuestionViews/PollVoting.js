49f4099654f60e9ad05aba11a0f89f5c8f4942fd
kassanj <kassandra@whereby.us>
2018-08-25 15:18:00 -0700

    fix handleOption functions

--- src/components/QuestionViews/PollVoting.js
+++ src/components/QuestionViews/PollVoting.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { handleQuestionAnswer} from '../../actions/shared'
 
 class PollVoting extends Component {
+
+  handleOptionOne = (e) => {
+    e.preventDefault()
+    const { dispatch, question } = this.props
+    const option = 'optionOne'
+    const id = question['id']
+    dispatch(handleQuestionAnswer(id, option))
+  }
+
+  handleOptionTwo = (e) => {
+    e.preventDefault()
+    const { dispatch, question } = this.props
+    const option = 'optionTwo'
+    const id = question['id']
+    dispatch(handleQuestionAnswer(id, option));
+  }
+
   render() {
 
     const { question } = this.props
@@ -12,8 +30,8 @@ class PollVoting extends Component {
          <div>{question.id}</div>
          <div>{question.author}</div>
          <div>Created: {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit'}).format(question.timestamp)}</div>
-         <div onClick={(e) => console.log(e)}>{question.optionOne.text}</div>
-         <div onClick={(e) => console.log(e)}>{question.optionTwo.text}</div>
+         <button onClick={this.handleOptionOne}>{question.optionOne.text}</button>
+         <button onClick={this.handleOptionTwo}>{question.optionTwo.text}</button>
    </li>
     )
   }
