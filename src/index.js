2c0096298dbc4aad2eb03f0747486e62d5811ce0
kassanj <kassandra@whereby.us>
2018-10-29 12:50:21 -0700

    feat: add Persist to Redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux'
+import { createStore, compose } from 'redux'
 import { Provider } from 'react-redux'
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+
 import './index.css';
 import App from './App';
-import reducers from './reducers'
+import reducer from './reducers'
 import middleware from './middleware'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
@@ -20,13 +24,23 @@ const theme = createMuiTheme({
   }
 });
 
-const store = createStore(reducers, middleware)
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(persistedReducer, composeEnhancers(middleware));
+const persistor = persistStore(store);
 
 ReactDOM.render(
   <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
     <MuiThemeProvider theme={theme}>
       <App />
     </MuiThemeProvider>
+    </PersistGate>
   </Provider>,
 document.getElementById('root')
 )
